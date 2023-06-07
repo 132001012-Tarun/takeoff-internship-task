8a6b1fbc72e15d75d07e0a44e0d6127ff333c5f2
132001012-Tarun <83160699+132001012-Tarun@users.noreply.github.com>
2023-06-07 20:12:08 +0530

    Update displaying.go

--- task1/displaying.go
+++ task1/displaying.go
@@ -1,1 +1,15 @@
+package main
 
+import "fmt"
+
+// login function prompts the user to enter username and password
+func printEmployeeDetails(employee Employee) { // basically a normal print function
+	fmt.Println("ID:", employee.ID)
+	fmt.Println("Username:", employee.Username)
+	fmt.Println("Role:", employee.Role)
+	fmt.Println("Name:", employee.Name)
+	fmt.Println("Email:", employee.Email)
+	fmt.Println("Phone:", employee.Phone)
+	fmt.Println("Birthdate:", employee.Birthdate.Format("2006-01-02"))
+	fmt.Println()
+}
