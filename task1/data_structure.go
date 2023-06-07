b81c218e5f52914e9afa4066c2d03155a69119be
132001012-Tarun <83160699+132001012-Tarun@users.noreply.github.com>
2023-06-07 20:14:02 +0530

    Update data_structure.go

--- task1/data_structure.go
+++ task1/data_structure.go
@@ -1,1 +1,22 @@
+package main
 
+import "time"
+
+type User interface {
+	getUsername() string
+	getRole() string
+}
+
+// Employee struct represents an employee record
+type Employee struct {
+	ID        int
+	Username  string
+	Password  string
+	Role      string
+	Name      string
+	Email     string
+	Phone     string
+	Birthdate time.Time
+}
+
+var employees []Employee // declaring employees as slice of Employee struct
